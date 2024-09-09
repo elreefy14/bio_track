fb6cdd9a1c07cd7b55629fbddfdb93d8f9f0d29c
elreefy14 <es-Ahmedhossameldin2024@alexu.edu.eg>
2024-09-09 03:13:43 +0300

    firebase image done

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,18 @@
-//i will change that app so that when user open app it will go to collection in firebase users doc phone then field faceJpg and save it in the local dbthen when user click button the identify button it will identify that image
+//i will change that app so that when user open app it will go to collection in firebase users doc phone then field faceJpg and save it in the local db
+//then when user click button the identify button it will identify that image
 
 import 'dart:convert';
 import 'dart:typed_data';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'dart:async';
 import 'dart:math';
 import 'package:facesdk_plugin/facesdk_plugin.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_exif_rotation/flutter_exif_rotation.dart';
+import 'package:local_auth/local_auth.dart';
 import 'package:path/path.dart';
 import 'package:sqflite/sqflite.dart';
 import 'package:fluttertoast/fluttertoast.dart';
@@ -20,7 +24,9 @@ import 'person.dart';
 import 'personview.dart';
 import 'facedetectionview.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -73,28 +79,52 @@ class MyHomePageState extends State<MyHomePage> {
         await _facesdkPlugin
             .setActivation(
             "CFO+UUpNLaDMlmdjoDlhBMbgCwT27CzQJ4xHpqe9rDOErwoEUeCGPRTfQkZEAFAFdO0+rTNRIwnQ"
-           "wpqqGxBbfnLkfyFeViVS5bpWZFk15QXP3ZtTEuU1rK5zsFwcZrqRUxsG9dXImc+Vw5Ddc9zBp9GE"
-           "UuDycHLqC9KgQGVb0TS2u9Kz67HQOSDw9hskjBpjRbqiG+F/h5DBLPzjgFh1Y6vzgg6I59FzTOcd"
-           "rdEbX7kI15Nwgf1hvHGtSgON/a0Fmw+XNdnxH2pVY96mcTemHYZAtxh8lA/t1DtTyZXpHjW8N6nq"
-           "4UN2YDlKLXSrDzLpLHJmBsdpH71AXb7dfAq94Q==")
+                "wpqqGxBbfnLkfyFeViVS5bpWZFk15QXP3ZtTEuU1rK5zsFwcZrqRUxsG9dXImc+Vw5Ddc9zBp9GE"
+                "UuDycHLqC9KgQGVb0TS2u9Kz67HQOSDw9hskjBpjRbqiG+F/h5DBLPzjgFh1Y6vzgg6I59FzTOcd"
+                "rdEbX7kI15Nwgf1hvHGtSgON/a0Fmw+XNdnxH2pVY96mcTemHYZAtxh8lA/t1DtTyZXpHjW8N6nq"
+                "4UN2YDlKLXSrDzLpLHJmBsdpH71AXb7dfAq94Q==")
             .then((value) => facepluginState = value ?? -1);
-    } else {
-    await _facesdkPlugin
-        .setActivation(
-    "nWsdDhTp12Ay5yAm4cHGqx2rfEv0U+Wyq/tDPopH2yz6RqyKmRU+eovPeDcAp3T3IJJYm2LbPSEz"
-    "+e+YlQ4hz+1n8BNlh2gHo+UTVll40OEWkZ0VyxkhszsKN+3UIdNXGaQ6QL0lQunTwfamWuDNx7Ss"
-    "efK/3IojqJAF0Bv7spdll3sfhE1IO/m7OyDcrbl5hkT9pFhFA/iCGARcCuCLk4A6r3mLkK57be4r"
-    "T52DKtyutnu0PDTzPeaOVZRJdF0eifYXNvhE41CLGiAWwfjqOQOHfKdunXMDqF17s+LFLWwkeNAD"
-    "PKMT+F/kRCjnTcC8WPX3bgNzyUBGsFw9fcneKA==")
-        .then((value) => facepluginState = value ?? -1);
-    }
+      } else {
+        await _facesdkPlugin
+            .setActivation(
+            "nWsdDhTp12Ay5yAm4cHGqx2rfEv0U+Wyq/tDPopH2yz6RqyKmRU+eovPeDcAp3T3IJJYm2LbPSEz"
+                "+e+YlQ4hz+1n8BNlh2gHo+UTVll40OEWkZ0VyxkhszsKN+3UIdNXGaQ6QL0lQunTwfamWuDNx7Ss"
+                "efK/3IojqJAF0Bv7spdll3sfhE1IO/m7OyDcrbl5hkT9pFhFA/iCGARcCuCLk4A6r3mLkK57be4r"
+                "T52DKtyutnu0PDTzPeaOVZRJdF0eifYXNvhE41CLGiAWwfjqOQOHfKdunXMDqF17s+LFLWwkeNAD"
+                "PKMT+F/kRCjnTcC8WPX3bgNzyUBGsFw9fcneKA==")
+            .then((value) => facepluginState = value ?? -1);
+      }
 
-    if (facepluginState == 0) {
-    await _facesdkPlugin
-        .init()
-        .then((value) => facepluginState = value ?? -1);
-    }
+      if (facepluginState == 0) {
+        await _facesdkPlugin
+            .init()
+            .then((value) => facepluginState = value ?? -1);
+      }
     } catch (e) {}
+
+    // Fetch data from Firebase Firestore
+    try {
+      QuerySnapshot snapshot = await FirebaseFirestore.instance.collection('users').get();
+      for (var doc in snapshot.docs) {
+        var data = doc.data() as Map<String, dynamic>;
+        String name = data['name'];
+
+        Uint8List faceJpg = base64Decode(data['faceJpg']);
+        Uint8List templates = base64Decode(data['templates']);
+
+        // Insert into the local database
+        Person person = Person(name: name, faceJpg: faceJpg, templates: templates);
+        await insertPerson(person);
+      }
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: "Error fetching data from Firebase: $e",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    }
 
     List<Person> personList = await loadAllPersons();
     await SettingsPageState.initSettings();
@@ -103,39 +133,35 @@ class MyHomePageState extends State<MyHomePage> {
     int? livenessLevel = prefs.getInt("liveness_level");
 
     try {
-    await _facesdkPlugin
-        .setParam({'check_liveness_level': livenessLevel ?? 0});
+      await _facesdkPlugin
+          .setParam({'check_liveness_level': livenessLevel ?? 0});
     } catch (e) {}
 
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
 
     if (facepluginState == -1) {
-    warningState = "Invalid license!";
-    visibleWarning = true;
+      warningState = "Invalid license!";
+      visibleWarning = true;
     } else if (facepluginState == -2) {
-    warningState = "License expired!";
-    visibleWarning = true;
+      warningState = "License expired!";
+      visibleWarning = true;
     } else if (facepluginState == -3) {
-    warningState = "Invalid license!";
-    visibleWarning = true;
+      warningState = "Invalid license!";
+      visibleWarning = true;
     } else if (facepluginState == -4) {
-    warningState = "No activated!";
-    visibleWarning = true;
+      warningState = "No activated!";
+      visibleWarning = true;
     } else if (facepluginState == -5) {
-    warningState = "Init error!";
-    visibleWarning = true;
+      warningState = "Init error!";
+      visibleWarning = true;
     }
 
     setState(() {
-    _warningState = warningState;
-    _visibleWarning = visibleWarning;
-    widget.personList = personList;
+      _warningState = warningState;
+      _visibleWarning = visibleWarning;
+      widget.personList = personList;
     });
   }
-
   Future<Database> createDB() async {
     final database = openDatabase(
       // Set the path to the database. Note: Using the `join` function from the
@@ -170,7 +196,6 @@ class MyHomePageState extends State<MyHomePage> {
       return Person.fromMap(maps[i]);
     });
   }
-
   Future<void> insertPerson(Person person) async {
     // Get a reference to the database.
     final db = await createDB();
@@ -189,7 +214,6 @@ class MyHomePageState extends State<MyHomePage> {
       widget.personList.add(person);
     });
   }
-
   Future<void> deleteAllPerson() async {
     final db = await createDB();
     await db.delete('person');
@@ -207,7 +231,6 @@ class MyHomePageState extends State<MyHomePage> {
         textColor: Colors.white,
         fontSize: 16.0);
   }
-
   Future<void> deletePerson(index) async {
     // ignore: invalid_use_of_protected_member
 
@@ -229,56 +252,130 @@ class MyHomePageState extends State<MyHomePage> {
         textColor: Colors.white,
         fontSize: 16.0);
   }
-
-  Future enrollPerson() async {
+  Future<void> enrollPerson(String phoneNumber) async {
     try {
       final image = await ImagePicker().pickImage(source: ImageSource.gallery);
       if (image == null) return;
 
-      var rotatedImage =
-      await FlutterExifRotation.rotateImage(path: image.path);
-
+      var rotatedImage = await FlutterExifRotation.rotateImage(path: image.path);
       final faces = await _facesdkPlugin.extractFaces(rotatedImage.path);
+
+      if (faces.isEmpty) {
+        Fluttertoast.showToast(
+          msg: "No face detected!",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 1,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+          fontSize: 16.0,
+        );
+        return;
+      }
+
       for (var face in faces) {
-        num randomNumber =
-            10000 + Random().nextInt(10000); // from 0 upto 99 included
-        print('\n\n\n\n\n');
-        print(face['faceJpg']);
-        print('\n\n\n');
-        print(face['faceJpg']);
-        print('\n\n\n');
-        print(face['templates']);
+        num randomNumber = 10000 + Random().nextInt(10000); // Random ID
 
+        // Convert faceJpg and templates to Uint8List or base64
+        Uint8List faceJpg = face['faceJpg'];
+        Uint8List templates = face['templates'];
 
-        Person person = Person(
-            name: 'Person' + randomNumber.toString(),
-            faceJpg: face['faceJpg'],
-            templates: face['templates']);
-            insertPerson(person);
+        // Store to Firebase Firestore
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc('01154354391') // Use phone number as document ID
+            .set({
+          'faceJpg': base64Encode(faceJpg),  // Save faceJpg as base64 string
+          'templates': base64Encode(templates),
+          'name': 'Person$randomNumber',
+        }, SetOptions(merge: true));  // Merge with existing data if needed
       }
 
-      if (faces.length == 0) {
-        Fluttertoast.showToast(
-            msg: "No face detected!",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.BOTTOM,
-            timeInSecForIosWeb: 1,
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0);
-      } else {
-        Fluttertoast.showToast(
-            msg: "Person enrolled!",
-            toastLength: Toast.LENGTH_SHORT,
-            gravity: ToastGravity.BOTTOM,
-            timeInSecForIosWeb: 1,
-            backgroundColor: Colors.red,
-            textColor: Colors.white,
-            fontSize: 16.0);
-      }
-    } catch (e) {}
+      Fluttertoast.showToast(
+        msg: "Person enrolled!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: "Error: $e",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        timeInSecForIosWeb: 1,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+        fontSize: 16.0,
+      );
+    }
   }
+  Future<void> enrollFingerprint(String phoneNumber) async {
+    final LocalAuthentication auth = LocalAuthentication();
+    try {
+      // Check if the device supports biometric authentication
+      bool canCheckBiometrics = await auth.canCheckBiometrics;
+      if (!canCheckBiometrics) {
+        Fluttertoast.showToast(
+          msg: "Biometric authentication not available",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+        );
+        return;
+      }
 
+      // Authenticate using biometrics (fingerprint or face recognition)
+      bool authenticated = await auth.authenticate(
+        localizedReason: 'Please authenticate to enroll fingerprint',
+        options: const AuthenticationOptions(
+          stickyAuth: true, // Keeps auth dialog open
+          biometricOnly: true, // Only allow biometrics
+        ),
+      );
+      if (!authenticated) {
+        Fluttertoast.showToast(
+          msg: "Authentication failed",
+          toastLength: Toast.LENGTH_SHORT,
+          gravity: ToastGravity.BOTTOM,
+          backgroundColor: Colors.red,
+          textColor: Colors.white,
+        );
+        return;
+      }
+
+      // Simulate the fingerprint data (since we can't access real fingerprint data)
+      String simulatedFingerprintData = base64Encode(utf8.encode("fingerprint_data_${DateTime.now()}"));
+
+      // Enroll the fingerprint in Firebase Firestore
+      await FirebaseFirestore.instance
+          .collection('users')
+          .doc('01154354391') // Use phone number as document ID
+          .set({
+        'fingerprint': simulatedFingerprintData,  // Save simulated fingerprint
+      }, SetOptions(merge: true));  // Merge with existing data
+
+      Fluttertoast.showToast(
+        msg: "Fingerprint enrolled successfully!",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.green,
+        textColor: Colors.white,
+      );
+    } catch (e) {
+      Fluttertoast.showToast(
+        msg: "Error: $e",
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+        backgroundColor: Colors.red,
+        textColor: Colors.white,
+      );
+    }
+  }
+//regigter new accont phone password login for admin
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -296,7 +393,8 @@ class MyHomePageState extends State<MyHomePage> {
                 child: ListTile(
                   leading: Icon(Icons.tips_and_updates),
                   subtitle: Text(
-                    'KBY-AI offers SDKs for face recognition, liveness detection, and id document recognition.',
+                    'KBY-AI offers SDKs for face recognition, '
+                        'liveness detection, and id document recognition.',
                     style: TextStyle(fontSize: 13),
                   ),
                 )),
@@ -322,7 +420,65 @@ class MyHomePageState extends State<MyHomePage> {
                             borderRadius:
                             BorderRadius.all(Radius.circular(12.0)),
                           )),
-                      onPressed: enrollPerson),
+                      onPressed: () {
+                        enrollPerson("01154354391"); // Replace with the actual phone number or pass it dynamically
+                      },
+                  ),
+                ),
+                const SizedBox(width: 20),
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton.icon(
+                      label: const Text('Identify'),
+                      icon: const Icon(
+                        Icons.person_search,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.only(top: 10, bottom: 10),
+                          backgroundColor:
+                          Theme.of(context).colorScheme.primaryContainer,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(12.0)),
+                          )),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => FaceRecognitionView(
+                                personList: widget.personList,
+                              )),
+                        );
+                      }),
+                ),
+              ],
+            ),  const SizedBox(
+              height: 6,
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 1,
+                  child: ElevatedButton.icon(
+                      label: const Text('Enroll fingerprint'),
+                      icon: const Icon(
+                        Icons.fingerprint,
+                        // color: Colors.white70,
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.only(top: 10, bottom: 10),
+                          // foregroundColor: Colors.white70,
+                          backgroundColor:
+                          Theme.of(context).colorScheme.primaryContainer,
+                          shape: const RoundedRectangleBorder(
+                            borderRadius:
+                            BorderRadius.all(Radius.circular(12.0)),
+                          )),
+                      onPressed: () {
+                        enrollFingerprint("01154354391");
+                        // Replace with the actual phone number or pass it dynamically
+                      },
+                  ),
                 ),
                 const SizedBox(width: 20),
                 Expanded(
@@ -461,8 +617,4 @@ class MyHomePageState extends State<MyHomePage> {
       ),
     );
   }
-}
-void printFullString(Uint8List uint8List) {
-  String decodedString = utf8.decode(uint8List);
-  print(decodedString);
 }
